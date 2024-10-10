46c0cf4aa85ea9be392372670efb43f1a5ffe94f
Liss <107251720+Himik0@users.noreply.github.com>
2024-10-10 11:46:45 +0000

    10/10

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -5,6 +5,11 @@ const res = await fetch(url)
 const dados = await res.json()
 console.log(dados); 
 const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = 'voce sabia que o mundo tem $(dados.total_pessoa_conectadas) e estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.'
+
+const container = document.getElementById ('grafico-container')
+container.appendChild(paragrafo)
 }
 
 visualizarInformacoeGlobais()
