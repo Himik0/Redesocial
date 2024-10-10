bb0717eada31795a4439f93c167755520804ceb8
Liss <107251720+Himik0@users.noreply.github.com>
2024-10-10 11:15:59 +0000

    10/10

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -1,5 +1,7 @@
 const ur = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function visualizarInformacoeGlobais(){
-
+const res = await fetch(url)
+const dados = await res.json()
+console.log(dados);
 }
